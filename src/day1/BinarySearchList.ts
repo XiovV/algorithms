998189d06c9a4583817b07c20a6cd45a09c9b17f
XiovV <xiovv@tutanota.com>
2023-12-26 18:55:14 +0100

    feat: implement binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,23 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length;
+
+  do {
+    const midpoint = Math.floor(lo + (hi - lo) / 2)
+    const val = haystack[midpoint]
+
+    if (val === needle) {
+      return true;
+    }
+
+    if (val > needle) {
+      hi = midpoint;
+    } else {
+      lo = midpoint + 1;
+    }
+
+  } while (lo < hi);
+
+  return false;
 
 }
